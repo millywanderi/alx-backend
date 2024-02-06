5b1c019ee9c032a7935022e09c3257f05a1fd717
millywanderi <wanderinyambura5@gmail.com>
2024-02-06 12:53:25 +0300

    Added the locale of the web page

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -32,7 +32,7 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """This is the home page"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
